249cf9708cda54330f9801c86df574f3dfb81353
Nettogrof <carl.lajeunesse@gmail.com>
2021-04-20 09:35:59 -0400

    Add comments and bit formatting

--- src/main/java/ai/nettogrof/battlesnake/proofnumber/FoodInfo.java
+++ src/main/java/ai/nettogrof/battlesnake/proofnumber/FoodInfo.java
@@ -6,39 +6,36 @@ import gnu.trove.iterator.TIntIterator;
 import gnu.trove.list.array.TIntArrayList;
 
 /**
+ * FoodInfo is the class that contain all the informations and related methods to foods
  * @author carl.lajeunesse
- *
+ * @version     %I%, %G%
  */
 public class FoodInfo {
-	//private short[][] food;
+	
 	/**
-	 * 
+	 * Arraylist (int) of all food position  (based on square formula)
 	 */
-	private transient  final TIntArrayList integers = new TIntArrayList();
+	private transient  final TIntArrayList position = new TIntArrayList();
 
-	/*public FoodInfo() {
-
-	}*/
-
+	
 	/**
-	 * @param board
+	 * Single constructor need the jsonNode board
+	 * @param board  JsonNode board element
 	 */
 	public FoodInfo(final JsonNode board) {
 		setFoodInfo(board.get("food"));
 	}
 
-	/*public short[][] getFood() {
-		return food;
-	}
-*/
+
 	/**
-	 * @param headX
-	 * @param headY
-	 * @return
+	 * Get the shortest distance from the position provided to a food.  Doesn't check Snake bodies/hazard/etc 
+	 * @param headX  The position X
+	 * @param headY  The position Y
+	 * @return int the number of square between the pos, and the nearest food
 	 */
 	public int getShortestDistance(final int headX, final int headY) {
 		int shortDis = Integer.MAX_VALUE;
-		final TIntIterator longIterator = integers.iterator();
+		final TIntIterator longIterator = position.iterator();
 		while (longIterator.hasNext()) {
 			final int foodpos = longIterator.next();
 			final int targetx = foodpos / 1000;
@@ -53,61 +50,53 @@ public class FoodInfo {
 		return shortDis;
 
 	}
-
+	
 	/**
-	 * @param board
+	 * Get the shortest distance from the position provided to a food.  Doesn't check Snake bodies/hazard/etc
+	 * @param headPos The square position (based on square formula)
+	 * @return int the number of square between the pos, and the nearest food
 	 */
-	public void setFood(final JsonNode board) {
-		setFoodInfo(board.get("food"));
+	public int getShortestDistance(final int headPos) {
+		return getShortestDistance(headPos / 1000,  headPos % 1000);
+		
 	}
 
+	
 	/**
-	 * @param squareX
-	 * @param squareY
-	 * @return
+	 * Check if there's a food on a particular square
+	 * @param squareX  Square position X
+	 * @param squareY  Square position Y
+	 * @return boolean if there's a food
 	 */
 	public boolean isFood(final int squareX, final int squareY) {
 
-		return integers.contains(squareX * 1000 + squareY);
+		return position.contains(squareX * 1000 + squareY);
 
 	}
+	
 	/**
+	 * Check if there's a food on a particular square (based on square formula)
 	 * @param pos
-	 * @return
+	 * @return boolean if there's a food
 	 */
 	public boolean isFood(final int pos) {
-
-		return integers.contains(pos);
-
+		return position.contains(pos);
 	}
+	
 
 	/**
-	 * @param foodI
+	 * Take the json field food and convert it into the arraylist position
+	 * 
+	 * @param foodArray  JsonNode field food
 	 */
-	private void setFoodInfo(final JsonNode foodI) {
-
-		for (int x = 0; x < foodI.size(); x++) {
-
-			integers.add(foodI.get(x).get("x").asInt() * 1000 + foodI.get(x).get("y").asInt());
+	private void setFoodInfo(final JsonNode foodArray) {
+		for (JsonNode foodPos : foodArray) {
+			position.add(foodPos.get("x").asInt() * 1000 + foodPos.get("y").asInt());
 
 		}
 
 	}
-	
-	
-	/*public boolean equals(final FoodInfo original) {
-		if (integers.size() != original.integers.size()) {
-			return false;
-		}
-		for (int x = 0 ; x <integers.size() ; x++ ) {
-			if (!original.isFood(integers.get(x))) {
-				return false;
-			}
-		}
-		return true;
-	}*/
-
-	
+		
 
 	/**
 	 *
@@ -124,11 +113,11 @@ public class FoodInfo {
 			return false;
 		}
 		final FoodInfo other = (FoodInfo) obj;
-		if (integers == null) {
-			if (other.integers != null) {
+		if (position == null) {
+			if (other.position != null) {
 				return false;
 			}
-		} else if (!integers.equals(other.integers)) {
+		} else if (!position.equals(other.position)) {
 			return false;
 		}
 		return true;
