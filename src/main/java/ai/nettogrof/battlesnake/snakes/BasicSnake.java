a57c71fd2b9989139b04e4d535c7c95a8d6ab1df
Nettogrof <carl.lajeunesse@gmail.com>
2021-04-21 16:54:57 -0400

    Add comments

--- src/main/java/ai/nettogrof/battlesnake/snakes/BasicSnake.java
+++ src/main/java/ai/nettogrof/battlesnake/snakes/BasicSnake.java
@@ -10,35 +10,53 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+/**
+ * Basic snake. This class is the "Nessegrev" snake on Battlesnake. a basic snake that avoid wall,
+ * and other snakes' bodies. And try to go the the nearest food, by going directly to it,
+ * doesn't check if their a snake between him and the food.
+ * 
+ * This snake should work only with API v1.
+ * 
+ * @author carl.lajeunesse
+ * @version Spring 2021
+ */
 public class BasicSnake extends AbstractSnakeAI {
 
+	/**
+	 * Basic / unused constructor
+	 */
 	public BasicSnake() {
 		super();
 	}
 
+	/**
+	 * Constructor with the gameid,
+	 * 
+	 * @param gameId String of the gameid field receive in the start request.
+	 */
 	public BasicSnake(final String gameId) {
 		super(gameId);
 
 	}
 
-	@Override
-	public Map<String, String> start(final JsonNode startRequest) {
-
-		final Map<String, String> response = new ConcurrentHashMap<>();
-		response.put("color", "#ff00ff");
-		response.put("headType", "sand-worm");
-		response.put("tailType", "sharp");
-		return response;
-	}
-
+	
+	/**
+	 * This method will be call on each move request receive by BattleSnake
+	 * @param moveRequest Json call received
+	 * @return map of field to be return to battlesnake,  example   "move" , "up"
+	 */
 	@Override
 	public Map<String, String> move(final JsonNode moveRequest) {
 		final Map<String, String> response = new ConcurrentHashMap<>();
 		final Map<String, Integer> possiblemove = new ConcurrentHashMap<>();
+		
+		//Put a value of 0 in each possible move
 		possiblemove.put(UPWARD, 0);
 		possiblemove.put(DOWN, 0);
 		possiblemove.put(LEFT, 0);
 		possiblemove.put(RIGHT, 0);
+		
+		//Create a board[][] filled with value 0 
 		final JsonNode boardJsonNode = moveRequest.get(BOARD);
 		width = boardJsonNode.get(WIDTH_FIELD).asInt();
 		height = boardJsonNode.get(HEIGHT_FIELD).asInt();
@@ -48,6 +66,8 @@ public class BasicSnake extends AbstractSnakeAI {
 				board[x][y] = 0;
 			}
 		}
+		
+		//Put value -99 for each snake body part
 		boardJsonNode.withArray(SNAKES).forEach(s -> {
 			s.withArray(BODY).forEach(c -> {
 				board[c.get("x").asInt()][c.get("y").asInt()] = -99;
@@ -58,9 +78,13 @@ public class BasicSnake extends AbstractSnakeAI {
 
 		final int snakex = moveRequest.get(YOU).withArray(BODY).get(0).get("x").asInt();
 		final int snakey = moveRequest.get(YOU).withArray(BODY).get(0).get("y").asInt();
-		int maxd[] = { 99 };
+		
+		
+		int maxd[] = { 99 }; // Max distance between snake head and food
 
 		int[] foodxy = new int[2];
+		
+		//Determine the location (foodxy) of the nearest food
 		boardJsonNode.withArray("food").forEach(f -> {
 			if (Math.abs(f.get("x").asInt() - snakex) + Math.abs(f.get("y").asInt() - snakey) < maxd[0]) {
 				maxd[0] = Math.abs(f.get("x").asInt() - snakex) + Math.abs(f.get("y").asInt() - snakey);
@@ -71,18 +95,30 @@ public class BasicSnake extends AbstractSnakeAI {
 		});
 		final int foodx = foodxy[0];
 		final int foody = foodxy[1];
+		
+		//Put value to each move based on nearest food
 		possiblemove.put(DOWN, snakey - foody);
 		possiblemove.put(UPWARD, foody - snakey);
 		possiblemove.put(LEFT, snakex - foodx);
 		possiblemove.put(RIGHT, foodx - snakex);
 
-		response.put("move", bestMove(snakex, snakey, possiblemove, board, height, width));
+		response.put("move", bestMove(snakex, snakey, possiblemove, board));
 		return response;
 	}
 
+	/**
+	 * Choose the best move 
+	 * @param snakex snake head X
+	 * @param snakey snake head Y 
+	 * @param possiblemove map of possible move
+	 * @param board the board[][] 
+	 * @return
+	 */
 	private String bestMove(final int snakex, final int snakey, final Map<String, Integer> possiblemove,
-			final int[][] board, final int boardHeight, final int boardWidth) {
-		if (snakey == boardHeight - 1) {
+			final int[][] board) {
+		
+		//For each direction , put value -90 if it's outside of the board, else  put the current value (based on food location)  + the board value (based on same body value  0 or - 99)
+		if (snakey == height - 1) {
 			possiblemove.put(UPWARD, -90);
 		} else {
 			possiblemove.put(UPWARD, possiblemove.get(UPWARD) + board[snakex][snakey + 1]);
@@ -100,12 +136,13 @@ public class BasicSnake extends AbstractSnakeAI {
 			possiblemove.put(LEFT, possiblemove.get(LEFT) + board[snakex - 1][snakey]);
 		}
 
-		if (snakex == boardWidth - 1) {
+		if (snakex == width - 1) {
 			possiblemove.put(RIGHT, -90);
 		} else {
 			possiblemove.put(RIGHT, possiblemove.get(RIGHT) + board[snakex + 1][snakey]);
 		}
 
+		//Choose the direction with the max value.
 		String res = UPWARD;
 		int value = possiblemove.get(UPWARD);
 
@@ -125,6 +162,10 @@ public class BasicSnake extends AbstractSnakeAI {
 	}
 
 	
+	/**
+	 * Return the infos need by Battlesnake when receive a (root GET /) request 
+	 * @return
+	 */
 	public static Map<String, String> getInfo() {
 
 		fileConfig = "Basic.properties";
@@ -152,10 +193,12 @@ public class BasicSnake extends AbstractSnakeAI {
 		return response;
 	}
 
+	/**
+	 *
+	 */
 	@Override
 	protected void setFileConfig() {
-		// TODO Auto-generated method stub
-
+		fileConfig = "Basic.properties";
 	}
 
 }
