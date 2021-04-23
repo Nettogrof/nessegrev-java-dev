7032beaaa708ea7b3a1c5018092132d7d4c745eb
Nettogrof <carl.lajeunesse@gmail.com>
2021-04-23 15:07:44 -0400

    Adding comments

--- src/main/java/ai/nettogrof/battlesnake/treesearch/search/squad/AbstractSquadSearch.java
+++ src/main/java/ai/nettogrof/battlesnake/treesearch/search/squad/AbstractSquadSearch.java
@@ -7,60 +7,110 @@ import ai.nettogrof.battlesnake.info.SnakeInfoSquad;
 import ai.nettogrof.battlesnake.treesearch.AbstractBestFirstSearch;
 import ai.nettogrof.battlesnake.treesearch.node.AbstractNode;
 
+/**
+ * This abstract Squad search, provide basic method use in any search in
+ * battlesnake squad mode
+ * 
+ * @author carl.lajeunesse
+ * @version Spring 2021
+ */
 public abstract class AbstractSquadSearch extends AbstractBestFirstSearch {
-	
+
+	/**
+	 * Basic constructor
+	 */
 	public AbstractSquadSearch() {
-		super();		
+		super();
 	}
-	
-	public AbstractSquadSearch(final AbstractNode root, final int width, final int heigth) {
+
+	/**
+	 * Constructor used to expand the tree once.
+	 * 
+	 * @param root   Root node
+	 * @param width  Board width
+	 * @param height Board height
+	 */
+	public AbstractSquadSearch(final AbstractNode root, final int width, final int height) {
 		super();
 		this.root = root;
 		this.width = width;
-		this.height = heigth;
-		
+		this.height = height;
 
 	}
 
+	/**
+	 * Constructor used to expand to do the tree search.
+	 * 
+	 * @param root      Root node
+	 * @param width     Board width
+	 * @param height    Board height
+	 * @param starttime starting time for the search in millisecond
+	 * @param timeout   the time limit to run the search
+	 */
+	public AbstractSquadSearch(final AbstractNode root, final int width, final int height, final long starttime,
+			final int timeout) {
+		super();
+		this.root = root;
+		this.width = width;
+		this.height = height;
+		this.startTime = starttime;
+		this.timeout = timeout;
+	}
+
+	/**
+	 * This method will be use to "kill" a snake
+	 * 
+	 * @param death SnakeInfo of the snake to kill
+	 * @param all   List of all snakeinfo
+	 */
 	@Override
-	public abstract void run() ;
-
-	@Override
-	protected void kill(final SnakeInfo death,final List<SnakeInfo> all ) {
-		kill((SnakeInfoSquad) death,  all );
-		
-		
+	protected void kill(final SnakeInfo death, final List<SnakeInfo> all) {
+		kill((SnakeInfoSquad) death, all);
 	}
 
-	
-	@Override
-	protected SnakeInfo createSnakeInfo(final SnakeInfo snake,final int newHead,final AbstractNode currentNode) {
-		return new SnakeInfo(snake, newHead, currentNode.getFood().isFood(newHead));
-	}
-
-	@Override
-	protected boolean freeSpace(final int square, final List<SnakeInfo> snakes,final SnakeInfo currentSnake) {
-		return freeSpace(square, snakes, (SnakeInfoSquad) currentSnake);
-		
-	}
-
-	protected boolean freeSpace(final int square, final List<SnakeInfoSquad> snakes,final SnakeInfoSquad currentSnake) {
-		boolean free = true;
-		for (int i = 0; i < snakes.size() && free; i++) {
-			free = snakes.get(i).equals(currentSnake) ? !snakes.get(i).isSnake(square)  :  !snakes.get(i).isSnake(square,currentSnake.getSquad()) ;			
-		}
-		return free;
-	}
-	
-	protected void kill(final SnakeInfoSquad death,final List<SnakeInfoSquad> all) {
+	/**
+	 * This method will be use to "kill" a snake in squad
+	 * 
+	 * @param death SnakeInfo of the snake to kill
+	 * @param all   List of all snakeinfo
+	 */
+	protected void kill(final SnakeInfoSquad death, final List<SnakeInfoSquad> all) {
 		death.die();
-		
 		for (final SnakeInfoSquad s : all) {
 			if (s.getSquad().equals(death.getSquad())) {
 				s.die();
 			}
 		}
-		
+
+	}
+
+	@Override
+	protected SnakeInfo createSnakeInfo(final SnakeInfo snake, final int newHead, final AbstractNode currentNode) {
+		return new SnakeInfo(snake, newHead, currentNode.getFood().isFood(newHead));
+	}
+
+	@Override
+	protected boolean freeSpace(final int square, final List<SnakeInfo> snakes, final SnakeInfo currentSnake) {
+		return freeSpace(square, snakes, (SnakeInfoSquad) currentSnake);
+
+	}
+
+	/**
+	 * Check if the snake can move on the square for squad mode
+	 * 
+	 * @param square       the int square
+	 * @param allSnakes    List of all snakes
+	 * @param currentSnake current Snake
+	 * @return boolean free to move on that square
+	 */
+	protected boolean freeSpace(final int square, final List<SnakeInfoSquad> snakes,
+			final SnakeInfoSquad currentSnake) {
+		boolean free = true;
+		for (int i = 0; i < snakes.size() && free; i++) {
+			free = snakes.get(i).equals(currentSnake) ? !snakes.get(i).isSnake(square)
+					: !snakes.get(i).isSnake(square, currentSnake.getSquad());
+		}
+		return free;
 	}
 
 }
--- src/main/java/ai/nettogrof/battlesnake/treesearch/search/squad/SquadSearch.java
+++ src/main/java/ai/nettogrof/battlesnake/treesearch/search/squad/SquadSearch.java
@@ -9,58 +9,70 @@ import java.util.List;
 import ai.nettogrof.battlesnake.info.SnakeInfo;
 import ai.nettogrof.battlesnake.info.SnakeInfoSquad;
 import ai.nettogrof.battlesnake.treesearch.node.AbstractNode;
+import ai.nettogrof.battlesnake.treesearch.search.royale.AbstractRoyaleNode;
 
 /**
+ * This squad search not used yet
+ * 
+ * 
  * @author carl.lajeunesse
- *
+ * @version Spring 2021
  */
 public class SquadSearch extends AbstractSquadSearch {
 
-	
 	/**
-	 * @param root
-	 * @param width
-	 * @param heigth
+	 * Constructor used to expand the tree once.
+	 * 
+	 * @param root   Root node
+	 * @param width  Board width
+	 * @param height Board height
 	 */
-	public SquadSearch(final AbstractNode root,final  int width,final int heigth) {
+	public SquadSearch(final AbstractNode root, final int width, final int heigth) {
 		super(root, width, heigth);
-		
+
 	}
 
-	
+	/**
+	 * Constructor used to expand to do the tree search.
+	 * 
+	 * @param root      Root node
+	 * @param width     Board width
+	 * @param height    Board height
+	 * @param starttime starting time for the search in millisecond
+	 * @param timeout   the time limit to run the search
+	 */
+	public SquadSearch(final AbstractRoyaleNode root, final int width, final int height, final long starttime,
+			final int timeout) {
+		super(root, width, height, starttime, timeout);
+	}
 
 	@Override
 	public void run() {
+
+		// TODO Squad Search Need to test if it work.
 		for (int i = 0; i < 12; i++) {
 			generateChild(getSmallestChild(root));
 		}
 		long currentTime = System.currentTimeMillis() - startTime;
 		List<AbstractNode> retNode = new ArrayList<>();
 		retNode.add(root);
-		
+
 		while (cont && currentTime < timeout && root.exp) {
-			//generateChild(retNode.get(0).getSmallestChild());// && root.getScoreRatio() > 0
+			// generateChild(retNode.get(0).getSmallestChild());// && root.getScoreRatio() >
+			// 0
 			final List<AbstractNode> bestChild = getBestPath(retNode.get(0));
 			generateChild(bestChild.get(0));
 			mergeList(bestChild, retNode);
 			retNode = updateListNode(bestChild);
 			Thread.yield();
 			currentTime = System.currentTimeMillis() - startTime;
-			
-		
-
 		}
 		updateFullListNode(retNode);
-
 	}
-
-	
-	
 
 	@Override
-	protected SnakeInfo createSnakeInfo(final SnakeInfo currentSnake,final  int newHead,final  AbstractNode node) {
-		return new SnakeInfoSquad((SnakeInfoSquad)currentSnake, newHead, node.getFood().isFood(newHead));
+	protected SnakeInfo createSnakeInfo(final SnakeInfo currentSnake, final int newHead, final AbstractNode node) {
+		return new SnakeInfoSquad((SnakeInfoSquad) currentSnake, newHead, node.getFood().isFood(newHead));
 	}
-
 
 }
