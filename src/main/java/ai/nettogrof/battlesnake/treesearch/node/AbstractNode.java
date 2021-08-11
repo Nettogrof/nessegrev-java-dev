3f12d4b86456152152d5c94d39d4f87f4ebab68f
Nettogrof <carl.lajeunesse@gmail.com>
2021-08-11 06:03:17 -0400

    bad commit please don'T look

--- src/main/java/ai/nettogrof/battlesnake/treesearch/node/AbstractNode.java
+++ src/main/java/ai/nettogrof/battlesnake/treesearch/node/AbstractNode.java
@@ -7,12 +7,10 @@ import ai.nettogrof.battlesnake.info.FoodInfo;
 import ai.nettogrof.battlesnake.info.HazardInfo;
 import ai.nettogrof.battlesnake.info.SnakeInfo;
 import ai.nettogrof.battlesnake.snakes.common.SnakeGeneticConstants;
-import gnu.trove.list.array.TIntArrayList;
+import ai.nettogrof.battlesnake.treesearch.decision.AbstractDecisionUtils;
+import ai.nettogrof.battlesnake.treesearch.decision.ParanoidUtils;
 
 import static ai.nettogrof.battlesnake.snakes.common.BattleSnakeConstants.BASIC_SCORE;
-import static ai.nettogrof.battlesnake.snakes.common.BattleSnakeConstants.INVALID_SCORE;
-import static ai.nettogrof.battlesnake.snakes.common.BattleSnakeConstants.MAX_SCORE;
-
 /**
  * This abstract node class is the based of all node class, provide basic method
  * use in any node.
@@ -36,6 +34,11 @@ public abstract class AbstractNode {
 	 * Single posssible move
 	 */
 	public final static int ONE = 1;
+	
+	/**
+	 * 
+	 */
+	public static AbstractDecisionUtils decision = new ParanoidUtils();
 
 	/**
 	 * List of child node
@@ -153,14 +156,7 @@ public abstract class AbstractNode {
 	 */
 	public void updateScore() {
 
-		if (!child.isEmpty()) {
-
-			if (possibleMove == ONE) {
-				updateScoreSinglePossibleMove();
-			} else {
-				updateScoreMultiplePossibleMove();
-			}
-		}
+		decision.updateScore(this);
 
 		updateScoreRatio();
 		updateChildCount();
@@ -182,112 +178,7 @@ public abstract class AbstractNode {
 		}
 	}
 
-	/**
-	 * Update score if more than 1 possible move
-	 */
-	private void updateScoreMultiplePossibleMove() {
-		final ArrayList<float[]> scores = new ArrayList<>();
-
-		initPayoffMatrix(scores);
-		computePayoffMatrix(scores);
-
-	}
-
-	/**
-	 * Compute the payoff Matric
-	 * 
-	 * @param scores List of score array
-	 */
-	private void computePayoffMatrix(final List<float[]> scores) {
-		final int ind = findBestIndex(scores);
-		if (ind > -1) {
-			for (int i = 0; i < scores.get(ind).length && scores.get(ind)[i] != -500; i++) {
-				score[i] = scores.get(ind)[i];
-			}
-
-			if (scores.get(ind).length < score.length) {
-				for (int i = scores.get(ind).length; i < score.length; i++) {
-					score[i] = (float) 0.0001;
-				}
-			}
-		} else {
-			for (int i = 0; i < score.length; i++) {
-				score[i] = 0;
-			}
-			for (final AbstractNode current : child) {
-				for (int j = 0; j < current.score.length; j++) {
-					score[j] += current.score[j];
-				}
-			}
-
-		}
-
-	}
-
-	/**
-	 * Find the best index in the payoff matrix
-	 * 
-	 * @param scores List of score array
-	 * @return int the index
-	 */
-	private int findBestIndex(final List<float[]> scores) {
-		int ind = -1;
-		float ration = -1;
-		for (int i = 0; i < scores.size(); i++) {
-			float other = 0;
-			for (int j = 1; j < scores.get(i).length; j++) {
-				if (scores.get(i)[j] != INVALID_SCORE) {
-					other += scores.get(i)[j];
-				}
-			}
-			final float currentratio = scores.get(i)[0] / other;
-			if (currentratio > ration) {
-				ration = currentratio;
-				ind = i;
-			}
-		}
-		return ind;
-	}
-
-	/**
-	 * Initiate the payoff matrix
-	 * 
-	 * @param scores List of score array
-	 */
-	private void initPayoffMatrix(final List<float[]> scores) {
-		final TIntArrayList head = new TIntArrayList();
-		for (final AbstractNode c : child) {
-			final int currentHead = c.getSnakes().get(0).getHead();
-			if (head.contains(currentHead)) {
-
-				float[] currentS = scores.get(head.indexOf(currentHead));
-				currentS[0] = c.score[0] < currentS[0] ? c.score[0] : currentS[0];
-				for (int i = 1; i < c.score.length; i++) {
-
-					currentS[i] = c.score[i] > currentS[i] ? c.score[i] : currentS[i];
-				}
-
-				if (score.length > c.score.length) {
-					for (int i = c.score.length; i < score.length; i++) {
-						currentS[i] = BASIC_SCORE;
-					}
-				}
-			} else {
-				head.add(currentHead);
-				float[] beta = { INVALID_SCORE, INVALID_SCORE, INVALID_SCORE, INVALID_SCORE, INVALID_SCORE,
-						INVALID_SCORE, INVALID_SCORE, INVALID_SCORE, INVALID_SCORE };
-				System.arraycopy(c.score, 0, beta, 0, c.score.length);
-
-				for (int i = c.score.length; i < score.length; i++) {
-					beta[i] = BASIC_SCORE;
-				}
-
-				scores.add(beta);
-			}
-
-		}
-
-	}
+	
 
 	/**
 	 * Update the child count
@@ -300,23 +191,7 @@ public abstract class AbstractNode {
 
 	}
 
-	/**
-	 * Update score if just one possible move.
-	 */
-	private void updateScoreSinglePossibleMove() {
-		for (int i = 1; i < score.length; i++) {
-			score[i] = 0;
-		}
-		score[0] = MAX_SCORE;
-		for (final AbstractNode current : child) {
-			score[0] = current.score[0] < score[0] ? current.score[0] : score[0];
-			for (int i = 1; i < current.score.length; i++) {
-
-				score[i] = current.score[i] > score[i] ? current.score[i] : score[i];
-			}
-		}
-
-	}
+	
 
 	/**
 	 * Gets list of child from this node
